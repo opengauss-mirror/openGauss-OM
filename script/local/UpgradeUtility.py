4a3be5dc282d2d2073c32311937066c6d42b71af
shirley_zhengx <zhengxue6@huawei.com>
2022-05-26 21:44:08 +0800

    delete description of parameter bgwriter_thred_num

--- script/local/UpgradeUtility.py
+++ script/local/UpgradeUtility.py
@@ -51,6 +51,7 @@ from gspylib.component.CM.CM_OLAP.CM_OLAP import CM_OLAP
 
 import impl.upgrade.UpgradeConst as const
 from base_utils.executor.cmd_executor import CmdExecutor
+from base_utils.os.crontab_util import CrontabUtil
 from base_utils.os.cmd_util import CmdUtil
 
 from base_utils.os.compress_util import CompressUtil
@@ -4437,6 +4438,41 @@ def kill_cm_server_process(gauss_home):
     subprocess.getstatusoutput(cmd)
     g_logger.debug("Kill cm_server finish.")
 
+    def cleanMonitor():
+    """
+    function: clean om_monitor process and delete cron
+    input : NA
+    output: NA
+    """
+    g_logger.debug("Deleting monitor.")
+    try:
+        # get all content by crontab command
+        (status, output) = CrontabUtil.getAllCrontab()
+        # overwrit crontabFile, make it empty.
+        crontabFile = "%s/gauss_crontab_file_%d" \
+                      % (EnvUtil.getTmpDirFromEnv(), os.getpid())
+        FileUtil.createFile(crontabFile, True)
+        content_CronTabFile = [output]
+        FileUtil.writeFile(crontabFile, content_CronTabFile)
+        FileUtil.deleteLine(crontabFile, "\/bin\/om_monitor")
+        CrontabUtil.execCrontab(crontabFile)
+        FileUtil.removeFile(crontabFile)
+
+        kill_monitor_cmd = DefaultValue.killInstProcessCmd("om_monitor")
+        g_logger.debug("Kill om_monitor command: {0}".format(kill_monitor_cmd))
+        status, output = subprocess.getstatusoutput(kill_monitor_cmd)
+        if status != 0:
+            g_logger.debug("Kill om_monitor process failed. Output: {0}".format(output))
+            raise Exception(ErrorCode.GAUSS_516["GAUSS_51606"] % "om_monitor")
+        g_logger.debug("Kill local om_monitor process successfully.")
+
+
+    except Exception as e:
+        if os.path.exists(crontabFile):
+            FileUtil.removeFile(crontabFile)
+        raise Exception(str(e))
+    g_logger.debug("Successfully deleted OMMonitor.")
+
 
 def clean_cm_instance():
     """
@@ -4451,6 +4487,9 @@ def clean_cm_instance():
     local_node = [node for node in cluster_info.dbNodes
                   if node.name == NetUtil.GetHostIpOrName()][0]
     clean_dir = os.path.dirname(local_node.cmagents[0].datadir)
+
+    cleanMonitor()
+    
     if local_node.cmservers:
         FileUtil.cleanDirectoryContent(local_node.cmservers[0].datadir)
         server_component = CM_OLAP()

607bd90a027df244eb3325960ac2e4e3a8515248
liuheng <hengliue@163.com>
2022-12-22 17:55:30 +0800

    修改om分配包，校验log的逻辑，解决预安装log目录不能是root用户

--- script/local/PreInstallUtility.py
+++ script/local/PreInstallUtility.py
@@ -1839,7 +1839,7 @@ Common options:
         tmp_path = ClusterConfigFile.readClusterTmpMppdbPath(self.user, self.clusterConfig)
         log_path = ClusterConfigFile.readClusterLogPath(self.clusterConfig)
         tool_path = self.clusterToolPath
-        for path in [app_path, tmp_path, log_path, tool_path]:
+        for path in [app_path, tmp_path, tool_path]:
             UserUtil.check_path_owner(path)
         # check cm, cn, gtm, etcd, dn
         # check cm

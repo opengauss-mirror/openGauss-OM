8b94fc97fc92ae26bc1966c844ca9d51029347f6
miffyrcee <miffyrcee@outlook.com>
2022-12-27 20:10:31 +0800

    fix upgrade failure

--- script/gspylib/component/BaseComponent.py
+++ script/gspylib/component/BaseComponent.py
@@ -53,6 +53,7 @@ class BaseComponent(object):
         self.level = 1
         self.clusterType = DefaultValue.CLUSTER_TYPE_SINGLE_INST
         self.paxos_mode = ''
+        self.dss_mode = ''
 
     def install(self):
         pass

b388e19be175aa9dfa6dae86aaf1445c127088b6
白凯 <kai.bai@enmotech.com>
2023-08-15 17:01:12 +0800

    fix:修复I7T0RB,增加PGPORT环境变量

--- script/local/PreInstallUtility.py
+++ script/local/PreInstallUtility.py
@@ -751,7 +751,7 @@ Common options:
         # user exists and input group not exists
         if userstatus == 0 and groupstatus != 0:
             self.logger.logExit(ErrorCode.GAUSS_503["GAUSS_50305"]
-                                + " User:Group[%s:%s]" 
+                                + " User:Group[%s:%s]"
                                 % (self.user, self.group))
 
         # user exists and group exists
@@ -1633,6 +1633,11 @@ Common options:
             datadir = node_info.datanodes[0].datadir
             FileUtil.writeFile(userProfile,
                              ["export PGDATA=%s" % datadir])
+
+            # set PGPORT
+            basePort = node_info.datanodes[0].port
+            FileUtil.writeFile(userProfile, ["export PGPORT=%d" % basePort])
+
             # set PATH
             if userProfile is ClusterConstants.ETC_PROFILE:
                 FileUtil.writeFile(userProfile, [
@@ -1650,6 +1655,8 @@ Common options:
                 "export LD_LIBRARY_PATH=$GPHOME/lib:$LD_LIBRARY_PATH"])
             # set PYTHONPATH
             FileUtil.writeFile(userProfile, ["export PYTHONPATH=$GPHOME/lib"])
+
+
         except Exception as e:
             self.logger.logExit(str(e))
         self.logger.debug("Successfully set tool ENV.")
@@ -2586,7 +2593,6 @@ Common options:
             shutil.rmtree(dest_path)
         os.makedirs(dest_path)
         FileUtil.changeOwner("root", dest_path)
-
         # cp $GPHOME script lib to /root/gauss_om/xxx
         cmd = ("cp -rf %s/script %s/lib %s/version.cfg %s"
                % (self.clusterToolPath, self.clusterToolPath,

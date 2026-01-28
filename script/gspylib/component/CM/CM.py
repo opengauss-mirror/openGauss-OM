450b565957d7907a8497a044eeeb7ea7e998726e
wangchao <zilan1986@163.com>
2026-01-28 11:25:02 +0800

    Revert "修复CmdUtil 导入问题"
    This reverts commit f58784c9bea8df7be0bae7bb5b871e477c7272c5.

--- script/gspylib/component/CM/CM.py
+++ script/gspylib/component/CM/CM.py
@@ -15,8 +15,6 @@
 # MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 # See the Mulan PSL v2 for more details.
 #############################################################################
-from base_utils.os.cmd_util import CmdUtil
-
 try:
     import sys
 

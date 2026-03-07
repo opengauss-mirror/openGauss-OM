c8e1e302414df7cabc37b6ddf2577f4ba9f5e2f9
zhangxubo <bxuzhang@163.com>
2026-03-07 14:22:43 +0800

    主机生成的hosts文件需要给备机也发送一份

--- script/impl/preinstall/PreinstallImpl.py
+++ script/impl/preinstall/PreinstallImpl.py
@@ -448,6 +448,7 @@ class PreinstallImpl:
             DefaultValue.distributeXmlConfFile(self.context.sshTool,
                                                hosts_file, hosts,
                                                self.context.mpprcFile)
+            self.context.sshTool.scpFiles(hosts_file, packageDir, hosts, self.context.mpprcFile)
         except Exception as e:
             raise Exception(str(e))
 

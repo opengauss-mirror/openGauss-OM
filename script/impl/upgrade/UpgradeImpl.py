74bc5979099198dd181eaa1d3bb868edb3519aff
Nerifish <649941490@qq.com>
2026-04-21 17:01:32 +0800

    hba upgrade fix

--- script/impl/upgrade/UpgradeImpl.py
+++ script/impl/upgrade/UpgradeImpl.py
@@ -2038,6 +2038,7 @@ class UpgradeImpl:
                         self.execRollbackUpgradedCatalog(scriptType="upgrade-post")
                         self.set_b_compatibility_param()
                         self.getLsnInfo()
+                self.reload_localhost_replication_hba()
                 hosts = copy.deepcopy(self.context.clusterNodes)
                 self.recordNodeStep(
                     GreyUpgradeStep.STEP_PRE_COMMIT, nodes=hosts)
@@ -2081,6 +2082,26 @@ class UpgradeImpl:
                     self.context.logger.debug("Successfully set guc {0} to {1}".format(guc_name, guc_value))
                 except Exception as e:
                     raise Exception(str(e))
+
+    def reload_localhost_replication_hba(self):
+        """
+        Reload localhost replication HBA entry before grey upgrade pre-commit.
+        """
+        self.context.logger.debug(
+            "Start to reload localhost replication HBA entry.")
+        hba_entry = "host    replication     {0}        " \
+                    "127.0.0.1/32            {1}".format(
+                        self.context.user, "trust")
+        cmd = "source {0} ; gs_guc reload -Iall -Nall -h '{1}'".format(
+            CmdUtil.quoteCmd(self.context.userProfile), hba_entry)
+        self.context.logger.debug(
+            "Command for reloading localhost replication HBA: %s." % cmd)
+        (status, output) = subprocess.getstatusoutput(cmd)
+        if status != 0:
+            raise Exception(ErrorCode.GAUSS_514["GAUSS_51400"] %
+                            "Command:%s. Error:\n%s" % (cmd, output))
+        self.context.logger.debug(
+            "Successfully reloaded localhost replication HBA entry.")
 
     def getOneNodeStep(self, nodeName):
         """
